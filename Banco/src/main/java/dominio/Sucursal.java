949b2c440df6c540777c7bf6eb5145c8e3b62edc
Martino Simon <msimon@gylgroup.com>
2026-04-16 11:12:55 -0300

    remueve imports innecesarios de sucursal

--- Banco/src/main/java/dominio/Sucursal.java
+++ Banco/src/main/java/dominio/Sucursal.java
@@ -4,10 +4,6 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
 public class Sucursal {
     private String codigo;
     private String nombre;
